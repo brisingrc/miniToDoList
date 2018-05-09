6ed0128c76f10927cafa06511cb61812e703c635
Алексей Ефимов <lex.efimov@gmail.com>
2018-05-09 14:16:28 +0500

    1. Начальный проект

--- Lesson 7/ViewController.swift
+++ Lesson 7/ViewController.swift
@@ -10,16 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var secondNameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        userNameLabel.isHidden = true
+    }
+    
+    func wrongFormatAlert() {
+        let alert = UIAlertController(title: "Wrong format!", message: "Please enter your name", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func donePressed(_ sender: UIButton) {
+        
+        guard firstNameTextField.text?.isEmpty == false else {
+            wrongFormatAlert()
+            return
+        }
+        guard secondNameTextField.text?.isEmpty == false else {
+            wrongFormatAlert()
+            return
+        }
+        
+        if let _ = Double(firstNameTextField.text!) {
+            wrongFormatAlert()
+        } else if let _ = Double(secondNameTextField.text!) {
+            wrongFormatAlert()
+        } else {
+            userNameLabel.isHidden = false
+            self.userNameLabel.text = firstNameTextField.text! + " " + secondNameTextField.text!
+        }
+        
+        firstNameTextField.text = nil
+        secondNameTextField.text = nil
     }
-
-
+    
 }
 
